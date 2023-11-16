65521303766fe4cfd33cd97680b5b2daba681c4c
SebastianKurrle <kurrlesebasitan@gmail.com>
2023-11-16 13:14:47 +0100

    Fixed overlay from practice room detail

--- frontend/src/components/Practice-Room/LanguagePracticeRoomDetailInfo.vue
+++ frontend/src/components/Practice-Room/LanguagePracticeRoomDetailInfo.vue
@@ -16,8 +16,10 @@ const optionsActivated = ref(false)
     <div
         class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{{ practiceRoomStore.currentPracticeRoom.name }}</h5>
-        <p class="mb-5 text-base text-left text-gray-500 sm:text-lg dark:text-gray-400">{{ practiceRoomStore.currentPracticeRoom.description }}</p>
         <p class="mb-5 text-base text-left text-gray-900 font-semibold sm:text-lg dark:text-gray-400">Language: {{ practiceRoomStore.currentPracticeRoom.language }}</p>
+        <div class="bg-gray-700 rounded-md p-3 mb-3">
+            <p class="text-base text-left text-gray-300 sm:text-lg dark:text-gray-400">{{ practiceRoomStore.currentPracticeRoom.description }}</p>
+        </div>
         <p class="mb-5 font-semibold text-blue-800 sm:text-lg">Learn new vocabularys</p>
         <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
             <button class="bg-gray-700 text-white p-3 rounded-md hover:bg-gray-800 mr-3">
@@ -32,6 +34,7 @@ const optionsActivated = ref(false)
         </div>
 
         <div class="mt-3" v-if="optionsActivated">
+            <h5 class="text-lg font-semibold text-blue-800 mb-3">Options</h5>
             <button 
                 class="bg-gray-800 p-3 w-32 rounded-md text-white hover:bg-gray-900 mr-3"
                 data-te-toggle="modal" data-te-target="#updateRoomModal" data-te-ripple-init data-te-ripple-color="dark"
